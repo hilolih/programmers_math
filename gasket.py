baaf3b8d1eba5cf6ab07577b453c4bbc0c21ad5f
hiroshi shimoda <hilolih@gmail.com>
2022-12-10 16:07:16 +0900

    わかりづらい部分をリファクタリング

--- gasket.py
+++ gasket.py
@@ -1,64 +1,71 @@
 import turtle
 from collections import deque
-len = 600
-stX = -250
-stY = -250
 
-#スタート地点に戻る
-def setHome():
-    t.setheading(0)
-    t.penup()
-    t.setposition(stX,stY)
-    t.pendown()
 #指定した場所に移動して方向を水平に変更
 def setXY(x,y):
-    t.setheading(0)
-    t.penup()
-    t.setposition(x,y)
-    t.pendown()
+  t.setheading(0)
+  t.penup()
+  t.setposition(x,y)
+  t.pendown()
 
 #最初に一度だけ描く大きい三角形
-def drawBigTri(l):
-    setHome()
-    t.fillcolor('green')
-    t.begin_fill()
-    for _ in range(3):
-        t.fd(l)
-        t.rt(240)
-    t.end_fill()
+def drawBigGreenTri(l=600):
+  t.fillcolor('green')
+  t.begin_fill()
+  for _ in range(3):
+    t.forward(l)
+    t.right(240)
+  t.end_fill()
 
 #再帰で小さい三角形を描画
-def drawTri(l):
-    l /= 2
-    if l < 5:
-      return
-    qq = []
-    for q in Q:
-        qq.append(q)
-    for q in qq:
-        setXY(q[0],q[1])
-        t.fd(l)
-        t.rt(300)
-        Q.append(list(t.position()))
-        t.fillcolor('white')
-        t.begin_fill()
-        for i in range(3):
-            if i == 2: Q.append(list(t.position()))
-            t.fd(l)
-            t.rt(240)
-        t.end_fill()
-    drawTri(l)
+def drawTri(l=600, limit=5):
+  l /= 2
+  # 三角形の辺が一定のサイズ以下になったら終了
+  if l < limit:
+    return
+  qq = list(Q)
+  for q in qq:
+    # 底辺真ん中に移動
+    setXY((q[0]+l),q[1])
+    triangle(l)
+  drawTri(l)
 
-# n回に一回しかアニメーションを更新しない -> 早くなる
+def triangle(l):
+  """
+  逆三角形を描画する
+  """
+  Q.append(list(t.position()))
+  t.begin_fill()
+  t.right(300)
+  for i in range(3):
+    if i == 2: Q.append(list(t.position()))
+    t.forward(l)
+    t.right(240)
+  t.end_fill()
 
+
+# len = 600
+stX = -250
+stY = -250
 t = turtle.Turtle()
 s = turtle.Screen()
+# n回に一回しかアニメーションを更新しない -> 早くなる
 s.tracer(8)
-
 t.speed(0)
-drawBigTri(len)
+
+t.setheading(0)
+t.penup()
+# スタートへ移動
+t.setposition(stX,stY)
+t.pendown()
+
+# 一番外側の大きな三角形を描画
+drawBigGreenTri()
 Q = deque()
 Q.append([stX,stY])
-drawTri(len)
+
+# 逆三角形はすべて白
+t.fillcolor('white')
+drawTri()
 
 turtle.done()
